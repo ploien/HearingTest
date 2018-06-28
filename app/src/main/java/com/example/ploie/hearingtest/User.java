7843187165e8463d012b362b772b017389405f46
arlundgren <drew@lundgren.us>
2018-06-28 14:38:24 -0600

    Modified the User class to implement Parcelable. The MainActivity has a mock user with a username, first name, and last name. It puts the parceled user into the intent as an extra, and when the TestActivity saves the data it pulls the currentUser's information from the parcel and passes it into firebase.

--- app/src/main/java/com/example/ploie/hearingtest/User.java
+++ app/src/main/java/com/example/ploie/hearingtest/User.java
@@ -1,14 +1,66 @@
 package com.example.ploie.hearingtest;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public int age;
+public class User implements Parcelable {
+
+    public String username;
     public String firstName;
     public String lastName;
 
-    public User(int age, String firstName, String lastName) {
-        this.age = age;
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public User(Parcel in) {
+        String[] data = new String[3];
+
+        in.readStringArray(data);
+
+        this.username = data[0];
+        this.firstName = data[1];
+        this.lastName = data[2];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {this.username,
+                                            this.firstName,
+                                            this.lastName});
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
