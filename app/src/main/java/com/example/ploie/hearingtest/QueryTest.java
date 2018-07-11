4d8029386f9ad5cb541fb09f2ee616c45102cc17
arlundgrenbyui <lun16002@byui.edu>
2018-07-10 23:02:41 -0600

    Working on Data Querying

---
+++ app/src/main/java/com/example/ploie/hearingtest/QueryTest.java
@@ -0,0 +1,33 @@
+package com.example.ploie.hearingtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class QueryTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_test);
+    }
+
+    public void onQueryClick(View view) {
+        Bundle data = getIntent().getExtras();
+        User CurrentUser = data.getParcelable("user");
+        Intent intent = new Intent(this, TestActivity.class);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("server/saving-data/fireblog/Tests/");
+        Query query = userRef.orderByKey().
+                      startAt("Fake Tester (Jul 6, 2018 12:42:54 PM)").
+                      endAt("Fake Tester (Jul 6, 2018 12:42:54 PM)");
+    }
+}
